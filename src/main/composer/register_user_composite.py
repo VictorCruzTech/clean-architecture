52958fbb48106adb4b7a95683501ed28c50251d0
Victor Cruz <gil.victor1998@gmail.com>
2024-07-09 19:10:20 -0300

    feat: implementing register_user ROute with Flask

--- src/main/composer/register_user_composite.py
+++ src/main/composer/register_user_composite.py
@@ -1,6 +1,6 @@
+from src.data.register_user.register import RegisterUserImpl
 from src.main.interface import RouteInterface
 from src.presenters.controllers import RegisterUserController
-from src.data.register_user import RegisterUserInterface
 from src.infra.repo.user_repository import UserRepository
 
 
@@ -10,7 +10,7 @@ def register_user_composer() -> RouteInterface:
     :return - Object with Register User Route
     """
     repository = UserRepository()
-    use_case = RegisterUserInterface(repository)
+    use_case = RegisterUserImpl(repository)
     register_user_route = RegisterUserController(use_case)
 
     return register_user_route
